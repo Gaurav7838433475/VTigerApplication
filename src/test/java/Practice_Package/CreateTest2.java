b54e61c1e6b89711ea60aab1a1886c460f458e88
gaurav7838433475 <gauravkumar92.badal@gmail.com>
2022-11-21 16:08:43 +0530

    class created in practise pack

---
+++ src/test/java/Practice_Package/CreateTest2.java
@@ -0,0 +1,11 @@
+package Practice_Package;
+
+public class CreateTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Suraj Arman");
+	}
+
+}
